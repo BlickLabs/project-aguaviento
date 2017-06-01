32fe51a371aede533f6d7b2a8a305a5fffe6cd8c
Salvador Hernández <zhavaman@gmail.com>
2017-06-01 13:30:19 -0500

    #9 add slick config

---
+++ src/js/slick.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.plant-carroussel').slick({
+    prevArrow: '<button type="button" data-role="none" class="slick-prev" aria-label="Previous" tabindex="0" role="button"></button>',
+    nextArrow: '<button type="button" data-role="none" class="slick-next" aria-label="Next" tabindex="0" role="button"></button>'
+  });
+});
