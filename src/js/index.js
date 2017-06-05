5a5fbf719b98b9823fea0e43eb1edc5595833b5e
MitchelHdz <sebastianvicky_@hotmail.com>
2017-06-05 18:39:59 -0500

    #2 Adding modal scripts

--- src/js/index.js
+++ src/js/index.js
@@ -4,35 +4,65 @@ document.querySelector('#neopraxis-hamburger-menu').addEventListener('click', fu
 });
 
 document.querySelector('#body').addEventListener('click', function (e) {
-	if (document.querySelector('#neopraxis-hamburger-menu').classList.contains('is-active') && e.target.tagName !== 'SPAN' && e.target.tagName !== 'a') {
+	if (document.querySelector('#neopraxis-hamburger-menu').classList.contains('is-active') && e.target.tagName !== 'SPAN' && e.target.tagName !== 'A') {
 		document.querySelector('#neopraxis-hamburger-menu').classList.remove('is-active');
 		document.querySelector('#neopraxis-main-navbar').classList.remove('neopraxis-navbar-style');
 	}
 });
 document.querySelector("#body").addEventListener( "touchstart",function (e) {
-	if (document.querySelector('#neopraxis-hamburger-menu').classList.contains('is-active') && e.target.tagName !== 'SPAN' && e.target.tagName !== 'a') {
+	if (document.querySelector('#neopraxis-hamburger-menu').classList.contains('is-active') && e.target.tagName !== 'SPAN' && e.target.tagName !== 'A') {
 		document.querySelector('#neopraxis-hamburger-menu').classList.remove('is-active');
 		document.querySelector('#neopraxis-main-navbar').classList.remove('neopraxis-navbar-style');
 	}
 });
-var imageModal = document.getElementById('imageModal');
+var servicesOneModal = document.getElementById('servicesOneModal');
+var servicesTwoModal = document.getElementById('servicesTwoModal');
 var imageClose = document.getElementById('imageClose');
-var imageBtn = document.getElementById("imageBtn");
+var imageCloseTwo = document.getElementById('imageCloseTwo');
+var imageBtnOne = document.getElementById("imageBtnOne");
+var imageBtnTwo = document.getElementById("imageBtnTwo");
 var body = document.getElementById('body');
-imageBtn.onclick = function(e) {
+imageBtnOne.onclick = function(e) {
 	e.preventDefault();
-	imageModal.classList.add('modal-open');
+	servicesOneModal.classList.add('modal-open');
 	body.classList.add('modal-open');
+	$('.servicesOne').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      nextArrow: '<img class="service-next cursor-pointer" src="./img/icons/group-3-copy-2@3x.png"></img>',
+      prevArrow: '<img class="service-prev cursor-pointer" src="./img/icons/group-3-copy-4@3x.png"></img>',
+    });
 }
 imageClose.onclick = function() {
-	imageModal.classList.remove('modal-open');
+	servicesOneModal.classList.remove('modal-open');
+	body.classList.remove('modal-open');
+}
+imageBtnTwo.onclick = function(e) {
+	e.preventDefault();
+	servicesTwoModal.classList.add('modal-open');
+	body.classList.add('modal-open');
+	$('.servicesTwo').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+      nextArrow: '<img class="service-next cursor-pointer" src="./img/icons/group-3-copy-2@3x.png"></img>',
+      prevArrow: '<img class="service-prev cursor-pointer" src="./img/icons/group-3-copy-4@3x.png"></img>',
+    });
+}
+imageCloseTwo.onclick = function() {
+	servicesTwoModal.classList.remove('modal-open');
 	body.classList.remove('modal-open');
 }
 window.onclick = function(event) {
-	if (event.target == imageModal) {
-		imageModal.classList.remove('modal-open');
+	if (event.target == servicesOneModal || event.target == servicesTwoModal ) {
+		servicesOneModal.classList.remove('modal-open');
+		servicesTwoModal.classList.remove('modal-open');
 		body.classList.remove('modal-open');
-		imageModal.style.display = "none";
+		servicesOneModal.style.display = "none";
+		servicesTwoModal.style.display = "none";
 	}
 }
 $(".click-scrolling ").on('click', function (event) {
